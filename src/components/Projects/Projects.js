69aba6654663a85478b399ba6ad4761ec26b3715
mrabaajr <mrabaajr@tip.edu.ph>
2025-05-17 21:23:55 +0800

    Initial commit of website

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ScrollToTop from "../ScrollToTop/ScrollToTop";
+import ProjectCard from "./ProjectCards";
+
+import PrivateChat from "../../Assets/Projects/PrivateChat.png";
+import Portfolio from "../../Assets/Projects/Portfolio.png";
+import Ecart from "../../Assets/Projects/ecart.png";
+import ExpenseTracker from "../../Assets/Projects/ExpenseTracker.png";
+
+import "./project.css";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="Fluorescent-Blue">Works </strong>
+        </h1>
+        <p>Here are a few projects I have worked on recently.</p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={6} lg={4} className="project-card">
+            <ProjectCard
+              imgPath={Portfolio}
+              title="Portfolio Website"
+              description="My personal Portfolio Website. It is fully responsive website which supports both dark and light mode."
+              ghLink="https://github.com/mrabaajr/Repos"
+            />
+          </Col>
+        </Row>
+      </Container>
+      <ScrollToTop />
+    </Container>
+  );
+}
+
+export default Projects;
